158ac333febece5346f4d3f20bd0099ac3355b4d
Daniel <danielsfmendes@gmail.com>
2017-03-15 23:29:12 +0000

    package manifest cleanup

--- package.js
+++ package.js
@@ -11,15 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.2.6');
-  api.use('ecmascript');
+  api.versionsFrom('1.2.1');
   api.mainModule('affiliates.js');
   api.export('Affiliates', 'client');
-});
-
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('danmendes:amazon-affiliate-links');
-  api.mainModule('affiliates-tests.js');
 });
